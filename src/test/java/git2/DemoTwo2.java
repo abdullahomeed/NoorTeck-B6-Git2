d69617455d057ec70bd01869775bafdadddc93d5
abdullahomeed <abdullahomeed@abdullameedsair.fios-router.home>
2022-05-05 20:24:07 -0400

    add method

--- src/test/java/git2/DemoTwo2.java
+++ src/test/java/git2/DemoTwo2.java
@@ -12,8 +12,8 @@ public static void main(String[] args) {
 System.out.println("random code 2...");
 }
 
-public void add(int a, int b) {
-	System.out.println(a+b);
+public void add(int a, int c) {
+	System.out.println(a+c);
 }
 }
 
