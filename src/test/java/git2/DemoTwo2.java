ef023074a575e5b058390fd6c9787f152aed9099
abdullahomeed <abdullahomeed@abdullameedsair.fios-router.home>
2022-05-05 20:15:24 -0400

    Second attempt

---
+++ src/test/java/git2/DemoTwo2.java
@@ -0,0 +1,17 @@
+package git2;
+
+public class DemoTwo2 {
+public static void main(String[] args) {
+	
+	System.out.println("Git demo 2...");
+	
+	System.out.println("random code...");
+	
+	System.out.println("random 1235");
+	
+	//test
+}
+}
+
+
+//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
